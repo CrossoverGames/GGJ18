3b7ec482f9295e8d5703a778663a9a5a3d03d1b4
Marcelo Vasques <msouvas@gmail.com>
2018-01-27 01:50:56 -0200

    Base for interface created

---
+++ game/Interface/interface.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+enum BTN_TYPE {
+	action = 0,
+	move_left = 1,
+	move_right = 2
+}
+
+onready var action_btn = get_node("action")
+onready var left_btn = get_node("left")
+onready var right_btn = get_node("right")
+
+signal btn_pressed
+
+func _ready():
+	action_btn.connect("pressed", self, "btn_pressed", [BTN_TYPE.action])
+	left_btn.connect("pressed", self, "btn_pressed", [BTN_TYPE.move_left])
+	right_btn.connect("pressed", self, "btn_pressed", [BTN_TYPE.move_right])
+	
+func btn_pressed(type):
+	print(type)
+	emit_signal("btn_pressed", type)
