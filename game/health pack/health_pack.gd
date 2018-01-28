c93166e742661bec29a9f041950a4be155fab3e9
Khin Baptista <khin.baptista@gmail.com>
2018-01-28 15:10:03 -0200

    Asteroid spawner also spawns health packs

--- game/health pack/health_pack.gd
+++ game/health pack/health_pack.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends "res://environment/EnvironmentObject.gd"
 
 func _ready():
 	$Area.connect("body_entered", self, "on_body_entered")
@@ -6,3 +6,4 @@ func _ready():
 func on_body_entered(body):
 	if body.is_in_group("ship"):
 		ship.heal()
+		queue_free()
