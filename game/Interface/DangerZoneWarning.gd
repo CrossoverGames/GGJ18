916ae5ec72f32816f95a91b751fc5e5f420096cc
Khin Baptista <khin.baptista@gmail.com>
2018-01-28 08:35:35 -0200

    Added warning when user goes too far to either side

--- game/Interface/DangerZoneWarning.gd
+++ game/Interface/DangerZoneWarning.gd
@@ -1,4 +1,11 @@
 extends Control
 
-func _ready():
-	pass
+export var time_to_disappear = 3.0
+
+func appear():
+	$anim.play("appear")
+	yield(get_tree().create_timer(time_to_disappear), "timeout")
+	disappear()
+
+func disappear():
+	$anim.play("disappear")
