02f268ebca00a16208a25b5e066b84ba5bb8c24b
Khin Baptista <khin.baptista@gmail.com>
2018-01-28 07:59:22 -0200

    Updated UI

--- game/Interface/cooldown.gd
+++ game/Interface/cooldown.gd
@@ -1,10 +1,17 @@
 extends TextureButton
 
 export var time = 2.0
+var timer
 
 func _ready():
-	pass
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.autostart = false
+	timer.connect("timeout", self, "set_disabled", [false])
+	add_child(timer)
+	connect("pressed", self, "activate_cooldown")
 
 func activate_cooldown():
 	self.disabled = true
-	
+	timer.wait_time = time
+	timer.start()
