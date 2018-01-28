4605cc3d449b2fa4daf45ceca9d520687a633ead
Khin Baptista <khin.baptista@gmail.com>
2018-01-27 23:00:09 -0200

    Added sattelite

---
+++ game/environment/sattelite/Sattelite.gd
@@ -0,0 +1,12 @@
+extends "../EnvironmentObject.gd"
+
+func _ready():
+	$Area.connect("body_entered", self, "on_body_enter")
+
+func on_body_enter(body):
+	if not body.is_in_group("ship"): return
+	body.elapsed_time = 0.0
+
+func _process(delta):
+	#$mesh.rotation_degrees.y += deg2rad(180) * delta
+	$mesh.rotate_y(deg2rad(180) * delta)
