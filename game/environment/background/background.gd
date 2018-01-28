3167cf825fecb8a3ee4f2d85943e3496d94cdf29
Khin Baptista <khin.baptista@gmail.com>
2018-01-27 23:35:33 -0200

    Stuff

--- game/environment/background/background.gd
+++ game/environment/background/background.gd
@@ -7,33 +7,36 @@ var moon2 = preload("res://environment/background/moon2.tscn")
 var moon3 = preload("res://environment/background/moon3.tscn")
 var belt = preload("res://environment/background/belt.tscn")
 
+export(NodePath) var ship_path
+
+var ship
+
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	if ship_path:
+		ship = get_node(ship_path)
 
 func _process(delta):
 	var chance = randi()%100
 	
-	if chance > (100 - draw_chance):
+	if (chance > (100 - draw_chance)) and ship.flying:
 		var rnd = randi()%4
-		var x_rnd = (randi()%20 - 10)
+		var x_rnd = (randi()%40 - 20)
 		
 		print(x_rnd)
 		
 		if rnd == 0:
 			var mn1 = moon1.instance()
 			add_child(mn1)
-			mn1.set_translation(Vector3(x_rnd, 0, 0))
+			mn1.set_translation(Vector3(x_rnd, 100, 0))
 		elif rnd == 1:
 			var mn2 = moon2.instance()
 			add_child(mn2)
-			mn2.set_translation(Vector3(x_rnd, 0, 0))
+			mn2.set_translation(Vector3(x_rnd, 100, 0))
 		elif rnd == 2:
 			var mn3 = moon3.instance()
 			add_child(mn3)
-			mn3.set_translation(Vector3(x_rnd, 0, 0))
+			mn3.set_translation(Vector3(x_rnd, 100, 0))
 		elif rnd == 3:
 			var blt = belt.instance()
 			add_child(blt)
-			blt.set_translation(Vector3(x_rnd, 0, 0))
+			blt.set_translation(Vector3(x_rnd, 100, 0))
